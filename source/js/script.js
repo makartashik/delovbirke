c6565c76e81585e23348655472bc774eac46f2e3
Natalia <wowmalish@gmail.com>
2019-01-24 00:26:10 +0300

    Размечает, стилизует блок с баннером, оживляет слайдер

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.page-banner').slick({
+    accessibility: true,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    cssEase: 'linear',
+  });
+});
