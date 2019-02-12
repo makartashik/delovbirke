63fcc736d9ac1284caadae7993eda11eb11e8cef
Natalia <wowmalish@gmail.com>
2019-02-12 15:42:09 +0300

    Добавляет блок с видео инструкциями

--- source/js/script.js
+++ source/js/script.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
     cssEase: 'linear',
   });
 });
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+       event.preventDefault();
+       var id  = $(this).attr('href'),
+       top = $(id).offset().top;
+       $('body,html').animate({scrollTop: top}, 2000);
+    });
+});
